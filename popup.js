5bef985a90a49e9fd440cf6b8933f70283407918
Trey B. <tbornottb@sbcglobal.net>
2014-11-24 13:38:55 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,13 +4,7 @@ function copy(name) {
     var copyDiv = document.getElementById(name);
 
     //Give the text element focus
-    copyDiv.focus();
-
-    //Select all content
-    document.execCommand('SelectAll');
-
-    //Copy Content
-    document.execCommand("Copy", false, null);
+    copyDiv.textContent.execCommand("Copy", false, null);
 }
 
 //Add Event Listeners to Button Click
