5c006e2f2ebd9794e2a9215176edadfa15ff44db
Trey B. <tbornottb@sbcglobal.net>
2014-11-25 15:51:59 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-function copyToClipboard( text ){
+function copyToClipboard( button ){
+                text = button.textContent;
                 var copyDiv = document.createElement('div');
                 copyDiv.contentEditable = true;
                 document.body.appendChild(copyDiv);
@@ -10,20 +11,3 @@ function copyToClipboard( text ){
                 document.body.removeChild(copyDiv);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    //var buttonList = document.getElementsByTagName('button')
-    //for(i = 0;i < buttonList.length; i++)
-    //{
-      // var copytext = buttonList[i].textContent;
-      // buttonList[i].onclick = copyToClipboard(copytext);
-    //}
-    
-    var button2 = document.getElementById("disapproval");
-    var copytext2 = button2.textContent;
-    button2.onclick = copyToClipboard("&#3232;_&#3232;");
-    
-    var button1 = document.getElementById("lenny");
-    var copytext1 = button1.textContent;
-    button1.onclick = copyToClipboard("( &#865;&#176; &#860;&#662; &#865;&#176;)");
-
-});
